8b6beba07252ab1e346fbd3b9f8035cd59193029
Mira Ehab <miraeha2@gmail.com>
2022-03-02 21:20:45 +0200

    Solved Problem of Day 2 (392. Is Subsequence)

---
+++ 392. Is Subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.length() == 0) return true;
+        int index = 0;
+        for(int i = 0; i<t.length(); i++){
+            if(index == s.length()) return true;
+            if(t[i] == s[index]){
+                index++;
+            }
+        }
+        if(index == s.length()) return true;
+        else return false;
+    }
+};
