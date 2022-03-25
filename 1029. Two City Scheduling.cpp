d7f0a503dffb5d1e73e06e788831b0f4e4cd7a65
Mira Ehab <miraeha2@gmail.com>
2022-03-25 21:21:21 +0200

    Solved Problem of Day 25 (1029. Two City Scheduling)

---
+++ 1029. Two City Scheduling.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        vector<int> diff;
+        int sum = 0;
+        for(int i = 0; i < costs.size(); ++i){
+            diff.push_back(costs[i][1]-costs[i][0]);
+            sum+=costs[i][0];
+        }
+        sort(diff.begin(), diff.end());
+        for(int i = 0; i < costs.size()/2; ++i){
+            sum+=diff[i];
+        }
+        return sum;
+    }
+};
